bcda930987a242684218bf6743326b1baf3fa2d7
赵家骏 <2670761096@qq.com>
2026-02-24 14:58:26 +0800

    提交文件

---
+++ api-passenger/src/main/java/com/mashibing/apipassenger/ApiPassengerApplication.java
@@ -0,0 +1,13 @@
+package com.mashibing.apipassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class ApiPassengerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ApiPassengerApplication.class);
+    }
+}
